62cea82ea191e7fb52e2b7399c5bec823dfd2e7a
sheetalkkharat <sheetalkharatk@gmail.com>
2024-08-29 18:14:30 -0500

    R2 Exercise

--- part2/exercises-2/src/components/Button.jsx
+++ part2/exercises-2/src/components/Button.jsx
@@ -7,6 +7,12 @@ function Button() {
             Learn More
          </button>
    );
+
+
+function onLearnMore() {
+   alert("Splish Splash!");
+ }
+ return <button onClick={onLearnMore}>Learn More</button>;
 }
 
 export default Button;
